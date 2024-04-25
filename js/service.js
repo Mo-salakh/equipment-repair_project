87f72c0dc61449489d2a191aca493566a75aaf7b
Mo-salakh <mokhmadsalakh.salamov@gmail.com>
2024-04-25 18:20:17 +0300

    swyper and show text

--- js/service.js
+++ js/service.js
@@ -1,24 +1,47 @@
-var swiper = new Swiper(".mySwiper", {
+
+  let firmsParent = document.querySelector('.service__firms');
+  let textShow = document.querySelector('.service__show-text');
+  let textChanged = textShow.lastElementChild;
+  let iconChanged = textShow.firstElementChild;
+  let isHidden = true;
+  
+  textShow.addEventListener('click', function() {
+    for (let i = 0; i < firmsParent.children.length; i++) {
+      if (firmsParent.children[i].classList.contains('hide')) {
+        firmsParent.children[i].style.display = isHidden ? 'flex' : 'none';
+      }
+    }
+    textChanged.textContent = isHidden ? 'Скрыть' : 'Показать';
+    if (isHidden) {
+      iconChanged.classList.add('icon-open');
+      iconChanged.classList.remove('icon-hide');
+    } else {
+      iconChanged.classList.add('icon-hide');
+      iconChanged.classList.remove('icon-open');
+    }
+    isHidden = !isHidden;
+  });
+
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+  
     slidesPerView: "auto",
-    spaceBetween: 30,
+    spaceBetween: 16,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+  
+    breakpoints: {
+      780: {
+        toggle: false,
+        loop: false,
+        spaceBetween: "auto",
+        slidesPerView: "auto",
+        centeredSlides: false,
+      },
+    },
   });
-
-  let h2 = document.querySelector("h2");
-  let swiperParent = h2.nextElementSibling;
-  let swiperWrapper = document.querySelector('.service__firms')
-  let swiperSlide = swiperParent.querySelector(".service__firm");
-  let pagination = document.querySelector("div.swiper-pagination");
 
   
-  window.addEventListener("resize", function () {
-    if (screen.width >= 780) {
-      swiperParent.classList.remove("swiper");
-      swiperParent.classList.remove("mySwiper");
-      swiperWrapper.classList.remove("swiper-wrapper");
-      swiperSlide.classList.remove("swiper-slide");
-    }
-  });
